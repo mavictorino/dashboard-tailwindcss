a79c0d38636f7b72427651414c78f1be07cfdff1
Mariana Victorino <victorino.marii@gmail.com>
2024-12-20 14:19:16 +0000

    add ProjectStatistics component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ProjectStatistics from '../components/ProjectStatistics'
+import Platforms from '../components/Platforms'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='p-5 '>
+      <div className='grid md:grid-cols-2 xl:grid-cols-4 gap-4'>
+        <ProjectStatistics />
+        <Platforms />
+        <ProjectStatistics />
+        <Platforms />
+        
+      </div>
+    </div>
   )
 }
 
